16801490773ce1dd8cc590b9743634be15818d3d
zhouzhe <zhouzhe@megvii.com>
2018-04-22 03:40:58 +0800

    update clean.py

--- clean.py
+++ clean.py
@@ -4,3 +4,4 @@ if __name__ == '__main__':
     shell.embeded_shell(['rm .*aux'])
     shell.embeded_shell(['rm makefile*'])
     shell.embeded_shell(['rm *.conf'])
+    shell.embeded_shell(['rm -r __pycache__'])
